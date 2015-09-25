67863d52435bfcdad47e011fe2caa55410ad1d7c
Trent Oswald <trentoswald@therebelrobot.com>
2015-09-25 10:26:50 -0700

    set the language to root

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
-app.get('/learn-badge/:lang', function(req, res, next){
+app.get('/:lang', function(req, res, next){
   console.log(req.params)
   res.redirect('https://img.shields.io/badge/Learn%20with%20APIs-'+req.params.lang+'-orange.svg');
 });
